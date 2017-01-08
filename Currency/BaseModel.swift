2efc346e64d0a105693a6868630e7590753cb899
S.Lance <lanchao0713@msn.com>
2017-01-08 18:23:28 +0800

    Add BaseModel

---
+++ Currency/BaseModel.swift
@@ -0,0 +1,13 @@
+//
+//  BaseModel.swift
+//  Currency
+//
+//  Created by Lance Blue on 08/01/2017.
+//  Copyright © 2017 Lance Blue. All rights reserved.
+//
+
+import UIKit
+
+class BaseModel: NSObject {
+
+}
